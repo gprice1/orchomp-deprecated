ef04e8d159f0c02cb006c1ea46b7404c58c5430e
Gene Price <gprice1@swarthmore.edu>
2014-06-21 15:48:53 -0400

    Added octree and kdtree stuff to distance field

--- src/orchomp_distancefield.cpp
+++ src/orchomp_distancefield.cpp
@@ -1,4 +1,12 @@
+
 #include "orchomp_distancefield.h"
+
+#define COLLISION -1
+#define NOCOLLISION 1
+#define XAXIS 0
+#define YAXIS 1
+#define ZAXIS 2
+
 
 namespace orchomp {
 
@@ -8,6 +16,61 @@ DistanceField::DistanceField() : aabb_padding(0.2), cube_extent(0.02)
 {
 }
 
+//a simple test function to see if two grids are the same;
+bool areEqual( DtGrid & first, DtGrid & second ){
+    for ( int i = 0; i < first.nx(); i ++ ){
+    for ( int j = 0; j < first.ny(); j ++ ){
+    for ( int j = 0; k < first.nz(); k ++ ){
+        if ( first(i,j,k) != second(i,j,k) ){
+            return false;
+        }
+    }
+    }
+    }
+}
+
+
+//several collision routines
+virtual inline bool DistanceField::isCollided( OpenRAVE::KinBodyPtr cube,
+                                          const OpenRAVE::Transform & world_to_cube ){
+
+    cube->SetTransform( world_to_cube );
+    if ( environment->CheckCollision( cube, kinbody ) ){
+        return true;
+    }
+
+    return false;
+}
+
+virtual bool DistanceField::isCollided( OpenRAVE::KinBodyPtr cube, int x1, int x2,
+                                            int y1, int y2,
+                                            int z1, int z2 )
+{
+    const int xdist = x2-x1;
+    const int ydist = y2-y1;
+    const int zdist = z2-z1;
+    
+    //get the offset from the index to the center of the cube.
+    OpenRAVE::Vector offset( xdist*cube_extent + x*cube_extent*2.0, 
+                             ydist*cube_extent + y*cube_extent*2.0,
+                             zdist*cube_extent + z*cube_extent*2.0);
+
+    OpenRAVE::Transform world_to_cube = pose_world * offset;
+
+    return isCollided( cube, world_to_cube );
+}
+
+virtual bool DistanceField::isCollided( OpenRAVE::KinBodyPtr cube, int x, int y, int z )
+{
+    OpenRAVE::Vector offset( cube_extent + x*cube_extent*2.0, 
+                             cube_extent + y*cube_extent*2.0,
+                             cube_extent + z*cube_extent*2.0);
+
+    OpenRAVE::Transform world_to_cube = pose_world * offset;
+
+    return isCollided( cube, world_to_cube );
+
+}
 void DistanceField::setupGrid(size_t x, size_t y, size_t z )
 {
 
@@ -55,6 +118,8 @@ DistanceField::createCube( OpenRAVE::EnvironmentBasePtr & env,
     return cube;
 
 }
+
+
 void DistanceField::createField( OpenRAVE::EnvironmentBasePtr & environment )
 {
  
@@ -100,10 +165,9 @@ void DistanceField::createField( OpenRAVE::EnvironmentBasePtr & environment )
                                             pose_world[2]   );
     
     //create a cube to do collision detection
-    std::string cube_name = "cube";
-    OpenRAVE::KinBodyPtr cube = createCube( environment,
-                                            pose_world,
-                                            cube_name);
+    std::string cube_name = "unitCube";
+    unitcube = createCube( environment, pose_world, cube_name);
+
 
     //setup the grid object for computing the occupancy grid,
     //  and the gradient and distance fields
@@ -111,6 +175,79 @@ void DistanceField::createField( OpenRAVE::EnvironmentBasePtr & environment )
 
     RAVELOG_INFO("computing occupancy grid ...\n");
     
+    
+    //fill the grid utilizing various methods.
+    simplefill();
+    octreefill( 0, grid.nx(), 0, grid.ny(), 0, grid.nz() );
+
+    Bound b;
+    b.bounds = { 0, grid.nx(), 0, grid.ny(), 0, grid.nz() };
+    b.dists  = { grid.nx() - 1, grid.ny() - 1 , grid.nz() - 1};
+    kdtreefill( b, XAXIS ); 
+    //Creates a distance field and gradient field from the binary values.
+    //  values above 0 are inside objects, 
+    //  values below 0 are outside of objects
+    grid.computeDistsFromBinary();
+    
+    //delete the cube , because we don't need this anymore
+    environment->Remove( unitCube );
+   
+}
+
+OpenRAVE::dReal DistanceField::getDist( const vec3 & trans, vec3 & gradient ){
+    
+    return grid.sample( trans, gradient );
+}
+
+
+
+OpenRAVE::KinBodyPtr DistanceField::createCube( int xdist, int ydist, int zdist )
+{
+
+    //create a cube to be used for collision detection in the world.
+    //  create an object and name that object 'cube'
+    OpenRAVE::KinBodyPtr cube = RaveCreateKinBody( env );
+
+    std::stringstream ss;
+    ss   << xdist << "_"
+         << ydist << "_"
+         << zdist ; 
+
+    std::name = ss.str()
+    cube->SetName( name.c_str() );
+
+    //set the dimensions of the cube object
+    std::vector< OpenRAVE::AABB > vaabbs(1);
+
+    /* extents = half side lengths */
+    vaabbs[0].extents = 
+        OpenRAVE::Vector(cube_extent*xdist,
+                         cube_extent*ydist,
+                         cube_extent*zdist );
+
+    cube->InitFromBoxes(vaabbs, 1);
+    
+    //add the cube to the environment
+    env->Add( cube );
+
+    return cube;
+
+}
+
+
+void DistanceField::setgrid( int x1, int x2, int y1, int y2, int z1, int z2, int value ){
+    for ( int i = x1; i < x2; i ++ ){
+        for ( int j = y1; j < y2; j ++){
+            for ( int k = z1; k < z2; k ++ ){
+
+                grid( i, j, k ) = value;
+            }
+        }
+    }
+}
+
+
+void DistanceField::simplefill(){
 
     //TODO make this do octree stuff
     //index over every point in the grid. and get the collision 
@@ -124,13 +261,14 @@ void DistanceField::createField( OpenRAVE::EnvironmentBasePtr & environment )
         OpenRAVE::Transform world_to_cube;
         getCenterFromIndex( i, j, k , world_to_cube);
 
-        cube->SetTransform( world_to_cube );
+        unitCube->SetTransform( world_to_cube );
         
         //check for collisions on the cube at the given transform
-        if ( environment->CheckCollision( cube, kinbody )){
+        if ( environment->CheckCollision( unitCube, kinbody )){
             //If there is a collision, set the value to 1
-            grid(i,j,k) = 1;
+            grid(i,j,k) = COLLISION;
             
+            //TODO remove this
             std::stringstream ss;
             ss << i << "_" << j << "_" << k;
             std::string nameofthecube = ss.str();
@@ -138,26 +276,180 @@ void DistanceField::createField( OpenRAVE::EnvironmentBasePtr & environment )
 
         }else {
             //If there is no collision, set the value to -1.
-            grid(i,j,k) = -1;
+            grid(i,j,k) = NOCOLLISION;
         }
     }
     }
     }
+}
+
+void DistanceField::octreefill( int x1, int x2, int y1, int y2, int z1, int z2 ){
+
+    const int xdist = x2 - x1;
+    const int ydist = y2 - y1;
+    const int zdist = z2 - z1;
     
-    //Creates a distance field and gradient field from the binary values.
-    //  values above 0 are inside objects, 
-    //  values below 0 are outside of objects
-    grid.computeDistsFromBinary();
+    //base case:
+    if ( xdist == 1 && ydist == 1 && zdist == 1){
+        if ( isCollided( unitCube, x1, y1, z1) ){
+            grid( x1,y1,z1 ) = COLLISION;
+        }else {
+            grid( x1,y1,z1 ) = NOCOLLISION;
+        }
+
+        return;
+    }
+        
+    //can we recurse over these dimensions?
+    const bool x_valid = ( xdist > 1 ? true : false );
+    const bool y_valid = ( ydist > 1 ? true : false );
+    const bool z_valid = ( zdist > 1 ? true : false );
     
-    //delete the cube , because we don't need this anymore
+    const int xmid = x1 + xdist/2 + 1;
+    const int xmid = y1 + ydist/2 + 1;
+    const int xmid = z1 + zdist/2 + 1;
+    
+
+    //check all of the squares:
+    cube = createcube( x1, xmid, y1, ymid, z1, zmid );
+
+    
+    //check for collision in each of the 8 different spaces
+
+    if( isCollided( cube, x1, xmid, y1, ymid, z1, zmid ) )
+    {
+        octreefill( x1, xmid, y1, ymid, z1, zmid );
+    }else {
+        setGrid( x1, xmid, y1, ymid, z1, zmid, NOCOLLISION );
+    }
+
+    if ( x_valid ){
+        if( isCollided( cube, xmid, x2, y1, ymid, z1, zmid ) )
+        {
+            octreefill( xmid, x2, y1, ymid, z1, zmid );
+        }else {
+            setGrid( xmid, x2, y1, ymid, z1, zmid, NOCOLLISION );
+        }
+    }
+
+    if ( y_valid ){
+        if( isCollided( cube, x1, xmid, ymid, y2, z1, zmid ) )
+        {
+            octreefill( x1, xmid, ymid, y2, z1, zmid );
+        }else {
+            setGrid( x1, xmid, ymid, y2, z1, zmid, NOCOLLISION );
+        }
+    }
+
+    if ( z_valid ){
+        if( isCollided( cube, x1, xmid, y1, ymid, zmid, z2 ) )
+        {
+            octreefill( x1, xmid, y1, ymid, zmid, z2 );
+        }else {
+            setGrid( x1, xmid, y1, ymid, zmid, z2, NOCOLLISION );
+        }
+    }
+
+    if ( x_valid && y_valid ){
+        if( isCollided( cube, xmid, x2, ymid, y2, z1, zmid ) )
+        {
+            octreefill( xmid, x2, ymid, y2, z1, zmid );
+        }else {
+            setGrid( xmid, x2, ymid, y2, z1, zmid, NOCOLLISION );
+        }
+    }
+
+    if ( y_valid && z_valid ){
+        if( isCollided( cube, x1, xmid, ymid, y2, zmid, z2 ) )
+        {
+            octreefill( x1, xmid, ymid, y2, zmid, z2 );
+        }else {
+            setGrid( x1, xmid, ymid, y2, zmid, z2, NOCOLLISION );
+        }
+    }
+
+    if ( z_valid && x_valid){
+        if( isCollided( cube, xmid, x2, y1, ymid, zmid, z2 ) )
+        {
+            octreefill( xmid, x2, y1, ymid, zmid, z2 );
+        }else {
+            setGrid( xmid, x2, y1, ymid, zmid, z2, NOCOLLISION );
+        }
+    }
+
+    if ( x_valid && y_valid && z_valid){
+        if( isCollided( cube, xmid, x2, ymid, y2, zmid, z2 ) )
+        {
+            octreefill( xmid, x2, ymid, y2, zmid, z2 );
+        }else {
+            setGrid( xmid, x2, ymid, y2, zmid, z2, NOCOLLISION );
+        }
+    }
+    
+    environment->Remove( cube )
+}
+
+
+
+void DistanceField::kdtreefill( Bound b, int AXIS ){
+
+    //Get the axis along which to split the space.
+    for ( int i = 0; i < 3; i ++ ){
+        if ( AXIS == ZAXIS ){ AXIS == XAXIS; }
+        else { AXIS++ ; }
+
+        //bail out when we find a dimension with greater than one dist.
+        if (b.dist[AXIS] > 1 ){ break; }
+    } 
+    
+    //base case : all of the Axes have a dist of 1, so it is just a single cube.
+    if (b.dist[AXIS] == 1 )
+    {
+        if ( isCollided( unitCube, b.bounds[0], b.bounds[1], b.bounds[2] ) ){
+            grid( b.bounds[0], b.bounds[0], b.bounds[0] ) = COLLISION;
+        }else{
+            grid( b.bounds[0], b.bounds[0], b.bounds[0] ) = NOCOLLISION;
+        }
+        return;
+    }
+
+
+    //If it is not a base case, split along the middle of the given dimension.
+    b.dist[AXIS] = b.dist[AXIS]/2 + 1;
+    const int midpoint = b.bounds[AXIS*2] + newdist;
+
+    OpenRAVE::KinBodyPtr cube = createCube( b.dists[0], b.dists[1], b.dists[2] );
+
+    /////////////Check the top half////////////////////////////////
+    const int temp = b.bounds[AXIS*2+1];
+    b.bounds[AXIS*2+1] = midpoint;
+
+    if ( isCollided(cube, b.bounds[0], b.bounds[1], b.bounds[2],
+                          b.bounds[3], b.bounds[4], b.bounds[5] ) )
+    {
+        kdtreefill( b, AXIS );
+    }else {
+        setgrid( b.bounds[0], b.bounds[1], b.bounds[2],
+                 b.bounds[3], b.bounds[4], b.bounds[5], NOCOLLISION );
+    }
+
+
+    /////////////Check the bottom half////////////////////////////////
+    //restore the original value of the upper bound;
+    b.bounds[AXIS*2+1] = temp;
+    b.bounds[AXIS*2] = midpoint;
+    b.dist[AXIS] = b.bounds[AXIS*2+1] - b.bounds[AXIS*2];
+
+    if ( isCollided( cube, b.bounds[0], b.bounds[1], b.bounds[2],
+                           b.bounds[3], b.bounds[4], b.bounds[5] ) )
+    {
+        kdtreefill( b, AXIS );
+    }else {
+        setgrid( b.bounds[0], b.bounds[1], b.bounds[2],
+                 b.bounds[3], b.bounds[4], b.bounds[5], NOCOLLISION );
+    }
+
     environment->Remove( cube );
-   
 }
-
-OpenRAVE::dReal DistanceField::getDist( const vec3 & trans, vec3 & gradient ){
-    
-    return grid.sample( trans, gradient );
-}
-
 
 } // namespace orchomp
--- src/orchomp_distancefield.h
+++ src/orchomp_distancefield.h
@@ -13,10 +13,22 @@ typedef vec3_t< OpenRAVE::dReal > vec3;
 
 class DistanceField{
 
+    //a small helper struct to keep track of bounds for the
+    //  kd tree fill.
+    typedef struct bounds_t {
+        int bounds[6];
+        int dists[3];
+
+    } Bound;
+
 public:
     
     //the kinematic body that the Distance field branches from.
     OpenRAVE::KinBodyPtr kinbody;
+
+    OpenRAVE::KinBodyPtr unitCube;
+    
+    OpenRAVE::EnvironmentBasePrt environment;
 
     //aabb_padding : the padding for the bounding box,
     //cube_extent : half of the width/height/depth of a voxel cell
@@ -45,6 +57,19 @@ public:
     
 
 private:
+
+
+
+    //various collision routines.
+    virtual inline bool isCollided( OpenRAVE::KinBodyPtr cube,
+                     const OpenRAVE::Transform & world_to_cube );
+    virtual bool isCollided( OpenRAVE::KinBodyPtr cube,
+                             int x1, int x2,
+                             int y1, int y2,
+                             int z1, int z2 );
+    virtual bool DistanceField::isCollided( OpenRAVE::KinBodyPtr cube,
+                                            int x, int y, int z );
+
     //gets the transform from the origin to the center of the cell at
     //  the specified indices.
     void getCenterFromIndex( size_t x, size_t y, size_t z,
@@ -54,16 +79,29 @@ private:
     //  distance field
     void setupGrid(size_t x, size_t y, size_t z );
     
+
     inline OpenRAVE::KinBodyPtr createCube(
                                 OpenRAVE::EnvironmentBasePtr & env,
                                 OpenRAVE::Vector & pos,
                                 std::string & name);
+    OpenRAVE::KinBodyPtr createCube( int xdist, int ydist, int zdist );
 
     void binaryFill();
     
+    void setgrid( int x1, int x2, int y1, int y2,
+                  int z1, int z2, int value );
+    
+    
+    //fills the grid by simple iterating over every point and checking
+    //  for collision.
+    void simplefill();
+    //two different methods of filling the binary collision grid.
+    //Use a recursive octree method to split the space repeatedly into 8 
+    //  squares
+    void octreefill( int x1, int x2, int y1, int y2, int z1, int z2 );
 
-
-
+    //use a recursive kdtree method to split space repeatedly in half. 
+    void kdtreefill( Bound b, int AXIS );
 };
 
 
